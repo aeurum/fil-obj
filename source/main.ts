dda27f856d49431b94530cd843ecca9b726099d8
Eaxon <aeurum@gmail.com>
2025-05-16 17:11:58 +0300

    Added analogous functions mutating the original object

--- source/main.ts
+++ source/main.ts
@@ -2,7 +2,11 @@ type Object = Record<string, any>
 type Filter = Function
 type Keys = Set<string> | Array<string>
 
-export function filter(object: Object, fn: Filter): Object {
+export function filter(object: Object, fn: Filter): void {
+  for (const key in object)
+    if (!fn(key, object[key], object)) delete object[key]
+}
+export function toFiltered(object: Object, fn: Filter): Object {
   const result: Object = { }
 
   for (const key in object)
@@ -12,6 +16,11 @@ export function filter(object: Object, fn: Filter): Object {
   return result
 }
 
+export function keepKeys(object: Object, keys: Keys): void {
+  const list = new Set(keys)
+  for (const key in object)
+    if (!list.has(key)) delete object[key]
+}
 export function includeKeys(object: Object, keys: Keys): Object {
   const result: Object = { }
 
@@ -20,6 +29,12 @@ export function includeKeys(object: Object, keys: Keys): Object {
       result[key] = object[key]
   
   return result
+}
+
+export function dropKeys(object: Object, keys: Keys): void {
+  const list = new Set(keys)
+  for (const key in object)
+    if (list.has(key)) delete object[key]
 }
 export function excludeKeys(object: Object, keys: Keys): Object {
   const result: Object = { }
